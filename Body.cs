d91293dcd744d8a183957815beab3cd2233446e9
Gulen <guleninsay@hotmail.co.uk>
2020-07-23 23:36:40 +0100

    Add files via upload

---
+++ Body.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+[RequireComponent(typeof(Rigidbody))]
+public class Body : MonoBehaviour
+{
+    public GameObject heavyBody;
+
+    Gravity gravity;
+
+
+    void Start() { gravity = heavyBody.GetComponent<Gravity>(); }
+
+    void FixedUpdate() { gravity.Pull(transform); }
+}
---
+++ Gravity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public float gravityFactor = -10f;
+
+    public void Pull(Transform body)
+    {
+        Vector3 targetDirection = (body.position - transform.position).normalized;
+        Vector3 bodyUpDirection = body.up;
+
+        body.rotation = Quaternion.FromToRotation(bodyUpDirection, targetDirection) *  body.rotation;
+        body.GetComponent<Rigidbody>().AddForce(targetDirection * gravityFactor);
+    }
+}
